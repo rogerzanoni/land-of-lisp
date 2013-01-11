96055c3771be1bee8fe1c4af8544210b33c6f78a
Roger Zanoni <roger.zanoni@openbossa.org>
2013-01-11 16:53:14 -0400

    Fixing indentation
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- text-game/text-game.lisp
+++ text-game/text-game.lisp
@@ -1,19 +1,19 @@
 
 (defparameter *nodes* '((living-room (you are in the living room. a wizard is snoring loudly in the couch.))
-			(garden (you are in a beautiful garden. there is a well in front of you.))
-			(attic (you are in the attic. there is a giant welding torch in the corner.))))
+                        (garden (you are in a beautiful garden. there is a well in front of you.))
+                        (attic (you are in the attic. there is a giant welding torch in the corner.))))
 
 (defparameter *edges* '((living-room (garden west door)
-				     (attic upstairs ladder))
-			(garden (living-room east door))
-			(attic (living-room downstairs ladder))))
+                         (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
 
 (defparameter *objects* '(whiskey frog bucket chain))
 
 (defparameter *object-locations* '((whiskey living-room)
-				   (bucket living-room)
-				   (chain garden)
-				   (frog garden)))
+                                   (bucket living-room)
+                                   (chain garden)
+                                   (frog garden)))
 
 (defparameter *location* 'living-room)
 
@@ -30,43 +30,43 @@
 
 (defun objects-at (loc objs obj-locs)
   (labels ((at-loc-p (obj)
-		     (eq (cadr (assoc obj obj-locs)) loc)))
+             (eq (cadr (assoc obj obj-locs)) loc)))
     (remove-if-not #'at-loc-p objs)))
 
 (defun describe-objects (loc objs obj-loc)
   (labels ((describe-obj (obj)
-			 `(you see a ,obj on the floor.)))
+             `(you see a ,obj on the floor.)))
     (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
 
 (defun look ()
   (append (describe-location *location* *nodes*)
-	  (describe-paths *location* *edges*)
-	  (describe-objects *location* *objects* *object-locations*)))
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
 
 (defun walk (direction)
   (let ((next (find  direction
-		     (cdr (assoc *location* *edges*))
-		     :key #'cadr)))
+                     (cdr (assoc *location* *edges*))
+                     :key #'cadr)))
     (if next
-	(progn (setf *location*  (car next))
-	       (look))
-      '(you cannot go that way.))))
+        (progn (setf *location*  (car next))
+               (look))
+        '(you cannot go that way.))))
 
 (defun pickup (object)
   (cond ((member object
-		 (objects-at *location* *objects* *object-locations*))
-	 (push (list object 'body) *object-locations*)
-	 `(you are now carrying the ,object))
-	(t '(you cannot get that))))
+                 (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body) *object-locations*)
+         `(you are now carrying the ,object))
+        (t '(you cannot get that))))
 
 (defun inventory ()
   (cons 'items- (objects-at 'body *objects* *object-locations*)))
 
 (defun game-read ()
   (let ((cmd (read-from-string
-	      (concatenate 'string "(" (read-line) ")"))))
+              (concatenate 'string "(" (read-line) ")"))))
     (flet ((quote-it (x)
-	     (list 'quote x)))
+             (list 'quote x)))
       (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
 
 (defun game-eval (sexp)
