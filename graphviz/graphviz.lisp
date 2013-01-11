88418c241689906f5def5e8d19517e831650a8f4
Roger Zanoni <roger.zanoni@openbossa.org>
2013-01-11 17:19:56 -0400

    Adding more graph functions
    Signed-off-by: Roger Zanoni <roger.zanoni@openbossa.org>

--- graphviz/graphviz.lisp
+++ graphviz/graphviz.lisp
@@ -60,3 +60,36 @@
   (dot->png fname
             (lambda ()
               (graph->dot nodes edges))))
+
+(defun ugraph->dot (nodes edges)
+  (princ "graph{")
+  (nodes->dot nodes)
+  (uedges->dot edges)
+  (princ "}"))
+
+(defun uedges->dot (edges)
+  (maplist (lambda (lst)
+             (mapc (lambda (edge)
+                     (unless (assoc (car edge) (cdr lst))
+                       (fresh-line)
+                       (princ (dot-name (caar lst)))
+                       (princ "--")
+                       (princ (dot-name (car edge)))
+                       (princ "[label=\"")
+                       (princ (dot-label (cdr edge)))
+                       (princ "\"];")))
+                   (cdar lst)))
+           edges))
+
+(defun dgraph->png (fname nodes edges)
+  (dot->png fname
+            (lambda ()
+              (dgraph->dot nodes edges))))
+
+(defun ugraph->png (fname nodes edges)
+  (dot->png fname
+            (lambda ()
+              (ugraph->dot nodes edges))))
+
+(defun run ()
+  (ugraph->png "wizard" *wizard-nodes* *wizard-edges*))
